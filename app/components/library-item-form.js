40437824664add37f58cc2e009939c8075213386
Jack Breakell <Jack@Admins-MacBook-Pro-2.local>
2016-09-21 09:53:02 +0100

    Add edit library button working

--- app/components/library-item-form.js
+++ app/components/library-item-form.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  buttonLabel: 'Save',
-
-  actions: {
-
-    buttonClicked(param) {
-      this.sendAction('action', param);
-    }
-
-  }
 });
